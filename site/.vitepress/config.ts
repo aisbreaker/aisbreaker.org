bc822941dc5e1e0a79c88a11c3c6cc1ba9738869
AIsBreaker <gh1@aisbreaker.org>
2023-12-05 22:55:17 +0100

    Added stats

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -8,6 +8,22 @@ export default withMermaid /*defineConfig*/ ({
   description: "AIsBreaker introduction and docs",
   lastUpdated: true,
   appearance: 'dark',
+  head: [
+    // for Google Analytics (G-Z1X0FSZ2Y0):
+    [
+      'script',
+      { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-Z1X0FSZ2Y0' }
+    ],
+    // for Google Analytics (G-Z1X0FSZ2Y0):
+    [
+      'script',
+      {},
+      `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-Z1X0FSZ2Y0');`
+    ]
+  ],
   
   sitemap: {
     hostname: 'https://aisbreaker.org',
